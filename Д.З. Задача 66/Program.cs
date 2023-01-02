2f02e7a8c6dc61b496044b112196bff063127463
Alexandr <milan.801813@gmail.com>
2023-01-02 22:02:22 +0300

    1

--- Д.З. Задача 66/Program.cs
+++ Д.З. Задача 66/Program.cs
@@ -1,41 +1,37 @@
 ﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму 
 //натуральных элементов в промежутке от M до N.
 
-/// <summary>
-        /// Рекурсивный запрос числа в консоли
-        /// </summary>
-        /// <param name="comment"></param>
-        /// <returns></returns>
+
         int GetDigit(string comment)
         {
             Console.WriteLine($"Введите {comment}:");
-            //Ввод пользователя
+            
             var d = Console.ReadLine();
             try
             {
-                //Попытка получить число из введенной строки
+                
                 var digit = Convert.ToInt32(d);
                 return digit;
             }
             catch (Exception)
             {
-                //Это не число, снова просим повторить ввод
+                
                 Console.WriteLine($"Ошибка, введенное значение {d} не является числом!");
                 return GetDigit(comment);
             }
         }
 
-        //Сумма чисел в интервале от n до m
+        
         int GetSumm(int n, int m)
         {
             int min = 0, max = 0;
-            //Числа равны - нечего считать
+            
             if (n == m)
                 return n + m;
 
             if (n > m)
             {
-                //Пользователь ошибся
+                
                 min = m;
                 max = n;
             }
@@ -48,7 +44,7 @@
             return Summ(min, max);
         }
 
-        //Рекурсия - расчет суммы между чисел
+        
         int Summ(int current, int max, int summ = 0)
         {
             if (current > max)
@@ -60,9 +56,9 @@
             return Summ(current, max, summ);
         }
 
-            //Рекурсивный ввод числа n
+            
             var n = GetDigit("первое число");
-            //Рекурсивный ввод числа n
+            
             var m = GetDigit("второе число");
 
             Console.WriteLine("Сумма натуральных чисел в интервале от {0} до {1} = {2}", n, m, GetSumm(n, m));
